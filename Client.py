e7eaf3618f4d0f0481ecf3f049633a0480a487a7
ntdat1107 <dat.ngo12345@hcmut.edu.vn>
2021-10-08 07:54:35 +0700

    test commit

--- Client.py
+++ Client.py
@@ -1,4 +1,4 @@
-#TEST
+#TEST111
 from tkinter import *
 import tkinter.messagebox
 from PIL import Image, ImageTk
@@ -14,12 +14,12 @@ class Client:
 	READY = 1
 	PLAYING = 2
 	state = INIT
-	
+
 	SETUP = 0
 	PLAY = 1
 	PAUSE = 2
 	TEARDOWN = 3
-	
+
 	def __init__(self, master, serveraddr, serverport, rtpport, filename):
 		self.master = master
 		self.master.protocol("WM_DELETE_WINDOW", self.handler)
@@ -34,9 +34,9 @@ class Client:
 		self.teardownAcked = 0
 		self.connectToServer()
 		self.frameNbr = 0
-		
-	# Initiatio
-	# THIS GUI IS JUST FOR REFERENCE ONLY, STUDENTS HAVE TO CREATE THEIR OWN GUI 	
+
+	# Initiation
+	# THIS GUI IS JUST FOR REFERENCE ONLY, STUDENTS HAVE TO CREATE THEIR OWN GUI
 	def createWidgets(self):
 		"""Build GUI."""
 		# Create Setup button
@@ -44,33 +44,33 @@ class Client:
 		self.setup["text"] = "Setup"
 		self.setup["command"] = self.setupMovie
 		self.setup.grid(row=1, column=0, padx=2, pady=2)
-		
-		# Create Play button		
+
+		# Create Play button
 		self.start = Button(self.master, width=20, padx=3, pady=3)
 		self.start["text"] = "Play"
 		self.start["command"] = self.playMovie
 		self.start.grid(row=1, column=1, padx=2, pady=2)
-		
-		# Create Pause button			
+
+		# Create Pause button
 		self.pause = Button(self.master, width=20, padx=3, pady=3)
 		self.pause["text"] = "Pause"
 		self.pause["command"] = self.pauseMovie
 		self.pause.grid(row=1, column=2, padx=2, pady=2)
-		
+
 		# Create Teardown button
 		self.teardown = Button(self.master, width=20, padx=3, pady=3)
 		self.teardown["text"] = "Teardown"
 		self.teardown["command"] =  self.exitClient
 		self.teardown.grid(row=1, column=3, padx=2, pady=2)
-		
+
 		# Create a label to display the movie
 		self.label = Label(self.master, height=19)
-		self.label.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5) 
-	
+		self.label.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5)
+
 	def setupMovie(self):
 		"""Setup button handler."""
 	#TODO
-	
+
 	def exitClient(self):
 		"""Teardown button handler."""
 	#TODO
@@ -78,43 +78,43 @@ class Client:
 	def pauseMovie(self):
 		"""Pause button handler."""
 	#TODO
-	
+
 	def playMovie(self):
 		"""Play button handler."""
 	#TODO
-	
-	def listenRtp(self):		
+
+	def listenRtp(self):
 		"""Listen for RTP packets."""
 		#TODO
-					
+
 	def writeFrame(self, data):
 		"""Write the received frame to a temp image file. Return the image file."""
 	#TODO
-	
+
 	def updateMovie(self, imageFile):
 		"""Update the image file as video frame in the GUI."""
 	#TODO
-		
+
 	def connectToServer(self):
 		"""Connect to the Server. Start a new RTSP/TCP session."""
 	#TODO
-	
+
 	def sendRtspRequest(self, requestCode):
-		"""Send RTSP request to the server."""	
+		"""Send RTSP request to the server."""
 		#-------------
 		# TO COMPLETE
 		#-------------
-		
-	
-	
+
+
+
 	def recvRtspReply(self):
 		"""Receive RTSP reply from the server."""
 		#TODO
-	
+
 	def parseRtspReply(self, data):
 		"""Parse the RTSP reply from the server."""
 		#TODO
-	
+
 	def openRtpPort(self):
 		"""Open RTP socket binded to a specified port."""
 		#-------------
@@ -122,10 +122,10 @@ class Client:
 		#-------------
 		# Create a new datagram socket to receive RTP packets from the server
 		# self.rtpSocket = ...
-		
+
 		# Set the timeout value of the socket to 0.5sec
 		# ...
-		
+
 
 	def handler(self):
 		"""Handler on explicitly closing the GUI window."""

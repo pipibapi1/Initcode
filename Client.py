8e7c2014e28de970458e699b94b60f439c23ee52
pipibapi1 <pipibapi1@gmail.com>
2021-10-12 11:33:12 +0700

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 22445102bf0b2d669fd993e20e5d55c6280826cb
->>>>>>> Stashed changes
 from tkinter import *
 import tkinter.messagebox
 from PIL import Image, ImageTk
@@ -71,44 +64,23 @@ class Client:
 
 		# Create a label to display the movie
 		self.label = Label(self.master, height=19)
-<<<<<<< Updated upstream
+
 		self.label.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5)
 
 #PART1#
 #########################################################################################################################################
 	def setupMovie(self):
-		"""Setup button handler."""
-	#TODO
-
-=======
-<<<<<<< HEAD
-		self.label.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5) 
-	
-	#1#
-	def setupMovie(self):
-		"""Setup button handler."""
+    		"""Setup button handler."""
 	#TODO
 	
-	#2#
-=======
-		self.label.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5)
-
-	def setupMovie(self):
-		"""Setup button handler."""
-	#TODO
-
->>>>>>> 22445102bf0b2d669fd993e20e5d55c6280826cb
->>>>>>> Stashed changes
 	def exitClient(self):
 		"""Teardown button handler."""
 	#TODO
-    
-	#3#
+
 	def pauseMovie(self):
 		"""Pause button handler."""
 	#TODO
-<<<<<<< Updated upstream
-
+	
 	def playMovie(self):
 		"""Play button handler."""
 	#TODO
@@ -117,125 +89,40 @@ class Client:
    #PART 2
 	#########################################################################################################################################
 
-	def listenRtp(self):
+	def listenRtp(self):		
 		"""Listen for RTP packets."""
 		#TODO
-
+					
 	def writeFrame(self, data):
 		"""Write the received frame to a temp image file. Return the image file."""
 	#TODO
-
+	
 	def updateMovie(self, imageFile):
 		"""Update the image file as video frame in the GUI."""
 	#TODO
-
-
+		
 	def connectToServer(self):
 		"""Connect to the Server. Start a new RTSP/TCP session."""
 	#TODO
+	
+	def sendRtspRequest(self, requestCode):
+		"""Send RTSP request to the server."""	
+		#-------------
+		# TO COMPLETE
+		#-------------
 
 #PART 3
 #################################################################################################################################################
 
-=======
-<<<<<<< HEAD
 	
-	#4#
-	def playMovie(self):
-		"""Play button handler."""
-	#TODO
-	
-	#5#
-	def listenRtp(self):		
-		"""Listen for RTP packets."""
-		#TODO
-
-	#6#				
-	def writeFrame(self, data):
-		"""Write the received frame to a temp image file. Return the image file."""
-	#TODO
-	
-	#7#
-	def updateMovie(self, imageFile):
-		"""Update the image file as video frame in the GUI."""
-	#TODO
-	
-	#8#
-	def connectToServer(self):
-		"""Connect to the Server. Start a new RTSP/TCP session."""
-	#TODO
-	
-	#9#
-=======
-
-	def playMovie(self):
-		"""Play button handler."""
-	#TODO
-
-	def listenRtp(self):
-		"""Listen for RTP packets."""
-		#TODO
-
-	def writeFrame(self, data):
-		"""Write the received frame to a temp image file. Return the image file."""
-	#TODO
-
-	def updateMovie(self, imageFile):
-		"""Update the image file as video frame in the GUI."""
-	#TODO
-
-	def connectToServer(self):
-		"""Connect to the Server. Start a new RTSP/TCP session."""
-	#TODO
-
->>>>>>> 22445102bf0b2d669fd993e20e5d55c6280826cb
->>>>>>> Stashed changes
-	def sendRtspRequest(self, requestCode):
-		"""Send RTSP request to the server."""
-		#-------------
-		# TO COMPLETE
-		#-------------
-<<<<<<< Updated upstream
-
-
-
 	def recvRtspReply(self):
-		"""Receive RTSP reply from the server."""
-		#TODO
-
-	def parseRtspReply(self, data):
-		"""Parse the RTSP reply from the server."""
-		#TODO
-
-=======
-<<<<<<< HEAD
-		
-	
-	#10#
-	def recvRtspReply(self):
-		"""Receive RTSP reply from the server."""
+    		"""Receive RTSP reply from the server."""
 		#TODO
 	
-	#11#
 	def parseRtspReply(self, data):
 		"""Parse the RTSP reply from the server."""
 		#TODO
 	
-	#12#
-=======
-
-
-
-	def recvRtspReply(self):
-		"""Receive RTSP reply from the server."""
-		#TODO
-
-	def parseRtspReply(self, data):
-		"""Parse the RTSP reply from the server."""
-		#TODO
-
->>>>>>> 22445102bf0b2d669fd993e20e5d55c6280826cb
->>>>>>> Stashed changes
 	def openRtpPort(self):
 		"""Open RTP socket binded to a specified port."""
 		#-------------
@@ -243,19 +130,11 @@ class Client:
 		#-------------
 		# Create a new datagram socket to receive RTP packets from the server
 		# self.rtpSocket = ...
-
+		
 		# Set the timeout value of the socket to 0.5sec
 		# ...
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 		
-    #13#
-=======
->>>>>>> Stashed changes
 
-
->>>>>>> 22445102bf0b2d669fd993e20e5d55c6280826cb
 	def handler(self):
 		"""Handler on explicitly closing the GUI window."""
 		#TODO

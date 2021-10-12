35271d7bea7934f53344b2741ac3252d927b7108
pipibapi1 <pipibapi1@gmail.com>
2021-10-12 16:48:12 +0700

    Update Client.py

--- Client.py
+++ Client.py
@@ -118,11 +118,58 @@ class Client:
 	def recvRtspReply(self):
     		"""Receive RTSP reply from the server."""
 		#TODO
+		while True :
+    			reply = self.rtspSocket.recv(1024)
+
+			if reply:
+    					self.parseRtspReply(reply)
+				# Close the RTSP socket upon requesting Teardown
+					if self.requestSent == self.TEARDOWN:
+    						self.rtspSocket.shutdown(socket.SHUT_RDWR)
+				   		self.rtspSocket.close()
+						break
 	
 	def parseRtspReply(self, data):
 		"""Parse the RTSP reply from the server."""
 		#TODO
-	
+		lines = data.decode().split('\n')
+		seqNum = int(lines[1].split(' ')[1])
+		
+		# Process only if the server reply's sequence number is the same as the request's
+		if seqNum == self.rtspSeq:
+			session = int(lines[2].split(' ')[1])
+			# New RTSP session ID
+			if self.sessionId == 0:
+				self.sessionId = session
+			
+			# Process only if the session ID is the same
+			if self.sessionId == session:
+				if int(lines[0].split(' ')[1]) == 200: 
+					if self.requestSent == self.SETUP:
+						#-------------
+						# TO COMPLETE
+						#-------------
+                        
+                        
+						# Update RTSP state.
+						self.state = self.READY
+						
+						# Open RTP port.
+						self.openRtpPort() 
+					elif self.requestSent == self.PLAY:
+						self.state = self.PLAYING
+					elif self.requestSent == self.PAUSE:
+						self.state = self.READY
+                        
+						
+						# The play thread exits. A new thread is created on resume.
+						self.playEvent.set()
+					elif self.requestSent == self.TEARDOWN:
+						self.state = self.INIT
+						
+						# Flag the teardownAcked to close the socket.
+						self.teardownAcked = 1 
+
 	def openRtpPort(self):
 		"""Open RTP socket binded to a specified port."""
 		#-------------
@@ -133,8 +180,20 @@ class Client:
 		
 		# Set the timeout value of the socket to 0.5sec
 		# ...
+		self.rtpSocket.settimeout(0.5)
 		
+		try:
+			# Bind the socket to the address using the RTP port given by the client user.
+			self.state=self.READY
+			self.rtpSocket.bind(('',self.rtpPort))
+		except:
+			messagebox.showwarning('Unable to Bind', 'Unable to bind PORT=%d' %self.rtpPort)
 
 	def handler(self):
 		"""Handler on explicitly closing the GUI window."""
 		#TODO
+		self.pauseMovie()
+		if messagebox.askokcancel("Quit?", "Are you sure you want to quit?"):
+			self.exitClient()
+		else: # When the user presses cancel, resume playing.
+			self.playMovie()
